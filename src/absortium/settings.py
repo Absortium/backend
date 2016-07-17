be48ca20e62620fca569f286925a26503d604bde
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-07-17 21:28:59 +0300

    add apikeyauth django app

--- src/absortium/settings.py
+++ src/absortium/settings.py
@@ -104,6 +104,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'core.apikeyauth.authentication.APIKeyAuth'
     )
 }
 
